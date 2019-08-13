39457e8a1962fb6c14d5ddc62f0dd827e6a8be7f
tototrinh <to.trinh@codeenginestudio.com>
2019-08-13 14:14:22 +0700

    LR_CoursePortlet_Finder: Create CourseFinder to get Course to the Course List with the total number of registrations for each Course

---
+++ docroot/WEB-INF/src/com/liferay/docs/course/service/persistence/CourseFinderImpl.java
@@ -0,0 +1,40 @@
+package com.liferay.docs.course.service.persistence;
+
+import java.util.List;
+
+import com.liferay.docs.course.model.Course;
+import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.dao.orm.Type;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.util.dao.orm.CustomSQLUtil;
+
+public class CourseFinderImpl extends BasePersistenceImpl<Course> implements CourseFinder {
+
+	public List<Object> getCoursesWithTotalRegistration()
+			throws SystemException {
+		
+		Session session = null;
+		try {
+			session = openSession();
+			String sql = CustomSQLUtil.get(GET_COURSE_WITH_TOTAL_REGISTRAION);
+			SQLQuery query = session.createSQLQuery(sql);
+
+			query.setCacheable(false);
+			query.addScalar("course_id", Type.LONG);
+			query.addScalar("course_name", Type.STRING);
+			query.addScalar("course_lecturer", Type.STRING);
+			query.addScalar("total_registration", Type.INTEGER);
+			QueryPos qPos = QueryPos.getInstance(query);
+			return (List<Object>) query.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static final String GET_COURSE_WITH_TOTAL_REGISTRAION = CourseFinder.class
+			.getName() + ".getCoursesWithTotalRegistration";
+}
