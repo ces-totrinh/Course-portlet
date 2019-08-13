34c2783515924058bb17ec84a308ea8a27e4db26
tototrinh <to.trinh@codeenginestudio.com>
2019-08-13 14:49:00 +0700

    LR_CoursePortlet_Listener: Create model listener to updates Course status to Full registered when have 20 Registration

---
+++ docroot/WEB-INF/src/com/liferay/custom/listener/MyRegistrationListener.java
@@ -0,0 +1,33 @@
+package com.liferay.custom.listener;
+
+import com.liferay.docs.course.model.Registration;
+import com.liferay.docs.course.service.CourseServiceUtil;
+import com.liferay.docs.course.service.RegistrationLocalServiceUtil;
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.model.BaseModelListener;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+public class MyRegistrationListener extends BaseModelListener<Registration>{
+	@Override
+	public void onAfterUpdate(Registration registration) throws ModelListenerException{
+		try{
+			long courseId = registration.getCourseId();
+			int totalRegistrationapproved = RegistrationLocalServiceUtil.countRegistrationApprovedByCourseId(courseId);
+			if(totalRegistrationapproved < MAXIMUM_NUMBER_OF_REGISTRATIONS_FOR_A_CLASS) return;
+			User user = UserLocalServiceUtil.getUser(registration.getUserId());
+			long groupId = user.getGroupId();
+			//Setting the status of the course is 'Unavailable' when there are 20 approved registration
+			CourseServiceUtil.updateStatusOfCourse(groupId, courseId, STATUS_UNAVAILALE_OF_COURSE);
+			//Setting the status of the remaining registrations is 'Rejected'
+			RegistrationLocalServiceUtil.updateRegistrationsWithRejectedStatus(courseId);
+			
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+		
+	}
+	
+	public static final boolean STATUS_UNAVAILALE_OF_COURSE = false;
+	public static final int MAXIMUM_NUMBER_OF_REGISTRATIONS_FOR_A_CLASS = 20;
+}
