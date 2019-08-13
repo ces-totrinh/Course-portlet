fd4be0db3609053e2898bf39e44bb44244155f9e
tototrinh <to.trinh@codeenginestudio.com>
2019-08-13 14:23:41 +0700

    LR_CoursePortlet_ActionKeys: Create Action key for Permission

---
+++ docroot/WEB-INF/src/com/liferay/docs/course/util/ActionKeys.java
@@ -0,0 +1,15 @@
+package com.liferay.docs.course.util;
+
+public class ActionKeys extends com.liferay.portal.security.permission.ActionKeys {
+	 public static final String ADD_COURSE = "ADD_COURSE";
+
+     public static final String ADD_REGISTRATION = "ADD_REGISTRATION";
+
+	 public static final String UPDATE_COURSE = "UPDATE_COURSE";
+     
+     public static final String UPDATE_REGISTRATION = "UPDATE_REGISTRATION";
+    
+	 public static final String DELETE_COURSE = "DELETE_COURSE";
+     
+     public static final String DELETE_REGISTRATION = "DELETE_REGISTRATION";
+}
